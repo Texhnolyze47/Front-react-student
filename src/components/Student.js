75e8568710f0fe1966a08809188c5ec91c8ccf97
Texhnolyze <ichigobankai136@gmail.com>
2022-07-02 23:05:24 -0500

    add: github pages pude mostrar la pagina

--- src/components/Student.js
+++ src/components/Student.js
@@ -17,7 +17,7 @@ export default function Student() {
     e.preventDefault();
     const student = {name, address};
     console.log(student);
-    fetch('http://localhost:8080/student/add', {
+    fetch('https://api-springv1.herokuapp.com/student/add', {
       method: 'POST',
       headers: {'Content-type': 'application/json'},
       body: JSON.stringify(student)
@@ -27,7 +27,7 @@ export default function Student() {
   }
 
   React.useEffect(() => {
-    fetch('http://localhost:8080/student/getAll')
+    fetch('https://api-springv1.herokuapp.com/student/getAll')
     .then(res => res.json())
     .then(result => {
       setStudents(result);
